dd4acf02da7018cf3144e368fca06455ab502922
Munchies123 <ycc9@sfu.ca>
2014-05-29 13:53:04 -0700

    Update OM signal Analysis_AP.m

--- OM signal Analysis_AP.m
+++ OM signal Analysis_AP.m
@@ -3,10 +3,10 @@
 % Filter Signal (cmd: butter, filtfilt, filter)
 data = [time,flipup(y)] %y value is either atrial or ventricle signal
 plot(data);
-[B,A] = butter(4,0.5); %where N = 4, sampling freq is 100Hz, creates a 25Hz filter (25/50 = 0.5)
+[B,A] = butter(4,0.75); %where N = 4, sampling freq is 100Hz, creates a mHz normalized filter cutoff (m/50 = 0.75)
 subplot(211),plot(data);
 set(gca,'YDir','reverse');
-subplot(212),plot(filter(B,A,data));
+subplot(212),plot(filtfilt(B,A,data));
 set(gca,'YDir','reverse');
 
 % Peak Analysis: Find Maxima (findpeaks)
